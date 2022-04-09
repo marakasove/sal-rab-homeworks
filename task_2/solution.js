d956ed9720fb8787615e1627d28e27dcb9c6d752
marakasove <marakas.e@gmail.com>
2022-04-09 18:45:00 +0400

    Task 2 is done for sure

--- task_2/solution.js
+++ task_2/solution.js
@@ -11,7 +11,7 @@ function calcShipping(sum, min, shipping) {
     }
     console.log(shippingSum);
     if (productsSum >= freeShippingMinSum) {// если productsSum Больше или равна freeShippingMinSum,
-        shippingSum = 0;// то shippingSum присвоить значение 0
+        shippingSum = 0;// то shippingSum присвоить значение 0 точно
     }
     if (productsSum > 0 && productsSum < freeShippingMinSum) {// если productsSum больше 0 и меньше freeShippingMinSum,
         shippingSum = shippingPrice; // то shippingSum присвоить значение shippingPrice
