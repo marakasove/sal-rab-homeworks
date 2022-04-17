08641b5883f70ff4fc76c324712c005e214624e3
marakasove <marakas.e@gmail.com>
2022-04-17 21:05:24 +0400

    Final task is finished and tested

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,20 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let client = name + " " + phone; 
+    address = 'ул. ' + address.street + ', дом '+address.house+', '+address.entrance+' подъезд, '+address.floor+' этаж, кв '+address.flat;
+    let order = {address, sum};
+
+    let data=({ client, order, goods: [] });
 
     let countOfGoods = goods.length;
+    
+    for (let i = 0; i < countOfGoods; i++) {
+        data.goods.push({ title: goods[i].title, count: goods[i].count });
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
